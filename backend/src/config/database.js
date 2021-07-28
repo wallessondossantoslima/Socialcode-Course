8b6bc6b7ecb2406617356bb10c5aa67ea17e0290
wallessondossantoslima <87343921+wallessondossantoslima@users.noreply.github.com>
2021-07-28 16:54:46 -0300

    w

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,11 +1,6 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "socialcode",
-});
+
 
 connection.connect((error) => {
   if (error) {
